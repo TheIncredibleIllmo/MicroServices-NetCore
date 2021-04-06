9a798e3ccd5c9b461a9468fb23bf2ac1196ad64c
Eduardo Medina <eduardo@soldsie.com>
2021-04-06 18:29:51 -0500

    Edited comment for Azure Pipeline test

--- ServicesStore.Api.BooksService.Tests/BooksServiceTest.cs
+++ ServicesStore.Api.BooksService.Tests/BooksServiceTest.cs
@@ -20,7 +20,7 @@ namespace ServicesStore.Api.BooksService.Tests
 
         private IEnumerable<LibraryMaterial> GetLibraryMaterials()
         {
-            //configures genfu to fill with fake data
+            //configures genfu to fill with fake data the tests
             A.Configure<LibraryMaterial>()
                 .Fill(x => x.Title).AsArticleTitle()
                 .Fill(x => x.LibraryMaterialId, () => Guid.NewGuid());
