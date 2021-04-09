6dfeab1930d02c8ec839a7f6a4e1df973802941f
Eduardo Medina <eduardo@soldsie.com>
2021-04-09 17:14:59 -0500

    Added:Docker implementation for containers and Ocelot

--- ServicesStore.Api.CartService/Startup.cs
+++ ServicesStore.Api.CartService/Startup.cs
@@ -26,6 +26,7 @@ namespace ServicesStore.Api.CartService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Important to add dependency services before!
             services.AddScoped<IBooksService, BooksService>();
 
             services
