4c91babd647b96664544a25055ccbe5477083f8c
Eduardo Medina <eduardo@soldsie.com>
2021-04-29 18:22:28 -0500

    'Added: Sendgrid implementation'

---
+++ ServicesStore.Messaging.Email/Sendgrid/Interfaces/ISendgridSend.cs
@@ -0,0 +1,49 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using ServicesStore.Messaging.Email.Sendgrid.Models;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesStore.Messaging.Email.Sendgrid.Interfaces
+{
+    public interface ISendgridSend
+    {
+        Task<(bool result, string errorMessage)> SendEmail(SendgridData data);
+    }
+
+    public class SendgridSend : ISendgridSend
+    {
+        public async Task<(bool result, string errorMessage)> SendEmail(SendgridData data)
+        {
+            try
+            {
+                var sendgridClient = new SendGridClient(data.SendgridApiKey);
+
+                var addressee = new EmailAddress(data.AddresseeEmail, data.AddresseeName);
+
+                var sender = new EmailAddress("eduardomedm@gmail.com", "Eduardo Medina");
+
+                var message = MailHelper.CreateSingleEmail(sender, addressee, data.Title, data.Content, data.Content);
+
+                var result = await sendgridClient.SendEmailAsync(message);
+                if (result.StatusCode != HttpStatusCode.OK)
+                {
+                    return (false, "Error when sending email...");
+                }
+
+                return (true, null);
+
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message);
+            }
+
+        }
+    }
+
+
+}
---
+++ ServicesStore.Messaging.Email/Sendgrid/Models/SendgridData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServicesStore.Messaging.Email.Sendgrid.Models
+{
+    public class SendgridData
+    {
+        public string SendgridApiKey { get; set; }
+
+        public string AddresseeEmail { get; set; }
+
+        public string AddresseeName { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+    }
+}
